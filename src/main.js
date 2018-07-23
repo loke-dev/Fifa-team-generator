18cd33d96464af0b89a07bc5a7446afdbf86f284
LokeCarlsson <carlssonloke@gmail.com>
2018-07-23 16:54:50 +0200

    Remove whitespaces

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import Vue from 'nativescript-vue';
-
 import router from './router';
-
 import store from './store';
-
 import './styles.scss';
 
 // Uncomment the following to see NativeScript-Vue output logs
-//Vue.config.silent = false;
+Vue.config.silent = false;
 
 new Vue({
-
   router,
-
   store,
-
 }).$start();
