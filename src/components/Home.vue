ef7f21295c7eeaee286d5c3af1645b9383b5185a
LokeCarlsson <carlssonloke@gmail.com>
2018-07-23 19:14:11 +0200

    Simplified app

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,41 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Home"/>
+    <ActionBar class="action-bar" title="Fifa">
+      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$router.push('/home')"/>
+    </ActionBar>
 
-    <StackLayout>
-      <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button>
-      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
-      <Button class="btn btn-primary" @tap="$router.push('/fifa')">Fifa</Button>
+    <StackLayout class="fifa-wrapper">
+      <Label class="body" textWrap=true text="This is a Fifa component"/>
+
+      <Button class="btn btn-primary" @tap="log" text="Tap me!"/>
     </StackLayout>
 
   </Page>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        surprise: false,
+      };
+    },
+    methods: {
+      log() {
+        console.log('Pressed the button!')
+      }
+    },
+  };
+</script>
+
+<style scoped>
+  .hello-world {
+    margin: 20;
+  }
+
+  Label {
+    color: green;
+    font-size: 25px;
+    text-align: center;
+  }
+</style>
